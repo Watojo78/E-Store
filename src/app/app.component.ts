53f4288f543bea6f75f11d62812177508285cc95
Wafo Tokam Joël <129383095+Watojo78@users.noreply.github.com>
2025-03-06 23:51:37 +0100

    chore: change language attribute to French and update component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'wtjoel-root',
+  selector: 'wtjoel',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
